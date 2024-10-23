dd68ab60b0e8350329500a18957b4cb4de5e657c
MykolaDekusar <ndekusar97@gmail.com>
2024-10-23 16:31:34 +0200

    Use of JOptionPane.showInputDialog to get input GUI and of JOptionPane.showMessageDialog to show with a GUI

--- GUI/src/GraphicalUserInterface.java
+++ GUI/src/GraphicalUserInterface.java
@@ -4,8 +4,11 @@ public class GraphicalUserInterface {
 
 	public static void main(String[] args) {
 		// Basic GUI
-
+					 //JOptionPane.showInputDialog permette di far apparire una GUI con input
 		String nome = JOptionPane.showInputDialog("Inserisci il tuo nome");
+		System.out.println(nome);
+		//JOptionPane.showMessageDialog mostra una GUI con i dati che vogliamo
+		JOptionPane.showMessageDialog(null, "Il tuo nome è: "+nome);
 	}
 
 }
